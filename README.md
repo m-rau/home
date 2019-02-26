ee1371c802eb72a1cacc945b20b7f3b2e1fe2bab
mra <tad)goW>
2019-02-26 15:11:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+home
+====
+
+Home and Housekeeping
---
+++ home/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.0.0'
+__project__ = 'core4'
+__built__ = None
+
+name = 'home'
+title = 'home (core4 project)'
+description = """Home and Housekeeping"""
